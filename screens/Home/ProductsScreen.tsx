6630bc502e116b285b80f20043ade8c5ebb2a9bb
BeakalDegefa <84756882+BeakalDegefa@users.noreply.github.com>
2022-10-03 17:30:55 -0400

    Products page layout and data rendered as per design

--- screens/Home/ProductsScreen.tsx
+++ screens/Home/ProductsScreen.tsx
@@ -1,49 +1,71 @@
-import { StyleSheet, Text, View, Button,TouchableOpacity,} from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, SafeAreaView } from "react-native";
 import React from "react";
-import { useNavigation, useRoute} from "@react-navigation/core";
+import { useNavigation, useRoute } from "@react-navigation/core";
 import { ScrollView } from "react-native-gesture-handler";
 import { RouteProp } from "@react-navigation/native";
+import { useProductsQuery } from "../../services/productsApi";
 
 type ParamList = { Params: { search: string } };
-
-
-
-
 const ProductsScreen = () => {
+  const { data: products, error, isLoading, isFetching, isSuccess } = useProductsQuery()
+  console.log(products);
   const navigation = useNavigation();
   const route = useRoute<RouteProp<ParamList, "Params">>();
-// const { search } = route.params;
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#dcb688" }}>
-      <Text>ProductsScreen</Text>
-      <Button
-        title="To Home"
-        onPress={() => navigation.navigate("HomeScreen" as never)}
-      />
-      <Button
-        title="To Product Details"
-        onPress={() => navigation.navigate("ProductDetailsScreen" as never)}
-      />
-      <View>
-        <ScrollView contentContainerStyle={styles.secondViewContainer}>
-      <TouchableOpacity style={styles.view2} onPress={() => navigation.navigate("ProductDetailsScreen" as never)}>
-         <View style={styles.imageBox}></View>
-          <View style={styles.categoryBox}>
-            <Text>Polo Shirt</Text>
-            <Text> review</Text>
-            <Text>$20</Text>
+    (<SafeAreaView style={{ flex: 1, backgroundColor: "#dcb688" }}>
 
+      {isLoading && <Text>...Loading </Text>}
+      {error && <Text>...Something went wrong </Text>}
+      {isSuccess && (<ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor: "#dcb688", padding: 10 }}>
+        <Text style={styles.title}>Our Products</Text>
+        {products && <View >
+          <View style={{ alignItems: "center" }}>
+            <View style={styles.firstContainer}>
+              {products?.map((product, index) => {
+                return (
+                  <View key={index}
+                  >
+                    <TouchableOpacity style={styles.productsView} onPress={() => navigation.navigate("ProductDetailsScreen" as never)}>
+                      <View style={styles.productsImageBox}>
+                        <Image
+                          resizeMode="cover"
+                          style={{
+                            width: 125,
+                            height: 100,
+                            borderRadius: 15,
+
+                            aspectRatio: 1,
+                          }}
+                          source={{ uri: `${product.image}` }}
+                        />
+                      </View>
+                      <View style={styles.productsBox}>
+
+                        <Text>{product.title}</Text>
+                        <Text>{product.rating.rate}</Text>
+                        <Text>${product.price}</Text>
+
+                      </View>
+                    </TouchableOpacity>
+
+                  </View>
+
+                )
+              })}
+
+            </View>
           </View>
-        </TouchableOpacity> 
-        
-        
-        </ScrollView>
-      </View>
 
-    </View>
-  );
+        </View>}
+
+      </ScrollView>)}
+    </SafeAreaView>
+    ));
 };
+
+
+
 
 export default ProductsScreen;
 
@@ -57,21 +79,11 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
   },
-  view1: {
-    backgroundColor: "white",
-    width: 125,
-    height: 135,
-    borderRadius: 15,
-    elevation: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
+
   secondViewContainer: {
     alignItems: "center",
   },
-  view2: {
+  productsView: {
     width: 350,
     height: 100,
     backgroundColor: "white",
@@ -84,14 +96,14 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 3,
   },
-  imageBox: {
+  productsImageBox: {
     width: 120,
     height: 100,
-    backgroundColor: "gray",
+    backgroundColor: "white",
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
   },
-  categoryBox: {
+  productsBox: {
     paddingLeft: 10,
     width: 175,
     height: 100,
