31399e8ba6b2138b87ee301bf21ac33d92509357
yelab07 <79489573+yelab07@users.noreply.github.com>
2022-10-05 17:10:07 -0500

    product layout worked on

--- screens/Home/ProductDetailsScreen.tsx
+++ screens/Home/ProductDetailsScreen.tsx
@@ -1,65 +1,69 @@
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Button, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { useNavigation, useRoute } from "@react-navigation/core";
 import { useProductQuery } from "../../services/productsApi";
 import { RouteProp } from "@react-navigation/native"
 type ParamList = { Params: { id: string }; };
+type Props = {
+  onPress: () => void;
+};
 
-
-const ProductDetailsScreen = () => {
+const ProductDetailsScreen = ({ onPress }: Props) => {
   const navigation = useNavigation();
   const route = useRoute<RouteProp<ParamList, "Params">>()
   const { id } = route.params
   const { data } = useProductQuery(id);
 
-
+  // onPress=()=>(Alert.alert("Success","Product has beed added to the cart "))
   return (
 
 
-    <View style={{ flex: 1, backgroundColor: "#dcb688" }}>
-
-
-      <Text>{data?.title}</Text>
-      <Text>{data?.category}</Text>
-
-      <Button
-        title="To Home"
-        onPress={() => navigation.navigate("HomeScreen" as never)}
-      />
-      <Button
-        title="To Products"
-        onPress={() => navigation.navigate("ProductsScreen" as never)}
-      />
-      <View style={{ alignItems: "center" }}>
-        < View style={styles.firstContainer}>
-
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
+    <View style={styles.container}>
 
 
 
-            <View style={styles.view1}
-            >
-              <Image resizeMode="cover"
-                style={{
-                  width: '100%',
-                  height: undefined,
-                  aspectRatio: 1,
-                }}
-                source={{ uri: `${data?.image}` }}
-              />
+      <ScrollView>
+        < View style={{ alignItems: 'center', marginHorizontal: 0 }}>
+
+          <View >
+            <View >
+              <Text style={styles.title}>{data?.title}</Text>
             </View>
-
+            <View style={{ flexDirection: 'row', justifyContent: "space-between", marginBottom: 20 }}>
+              <Text style={styles.category}>{data?.category}</Text>
+              <Text> Rating:{data?.rating.rate}</Text>
+            </View>
+          </View>
+          <View
+          >
+            <Image resizeMode="cover"
+              style={styles.productImg}
+              source={{ uri: `${data?.image}` }}
+            />
           </View>
 
+          <View style={{ flexDirection: 'row', justifyContent: "space-between", flex: 1, width: 250 }}>
+            <View >
+              <Text style={styles.price}> ${data?.price}</Text>
+              <Text style={{ fontSize: 20, alignContent: 'center' }}>In stock</Text>
+
+            </View>
+            <View>
+              {/* <Text>Qty</Text> */}
+              {/* <QuantitySelector quantity={quantity} setQuantity={setQuantity} /> */}
+            </View>
+          </View>
 
         </View>
-      </View>
+        <View style={styles.addToCartWrapper}>
+          <TouchableOpacity style={styles.addButton}
+          // onPress={()=>clickEventListener()}
+          >
+            <Text style={styles.addButtonText}>Add To Cart</Text>
+          </TouchableOpacity>
+        </View>
+        <View></View>
+      </ScrollView>
     </View>
   );
 };
@@ -67,58 +71,41 @@ const ProductDetailsScreen = () => {
 export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: "#dcb688"
+  },
+  productImg: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
   title: {
+    marginBottom: 10,
     fontSize: 25,
-    padding: 15,
+    fontWeight: 'bold'
   },
-  firstContainer: {
-    flexDirection: "column",
-    width: 300,
-    height: 300,
+  category: {
+    fontSize: 20,
+  },
+  price: { fontSize: 20 },
+  addToCartWrapper: {
 
   },
-  view1: {
-    backgroundColor: "white",
-    width: 600,
-    height: 600,
-    borderRadius: 15,
-    elevation: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    marginTop: 40,
-  },
-  secondViewContainer: {
-    alignItems: "center",
-  },
-  view2: {
-    width: 350,
-    height: 100,
-    backgroundColor: "white",
-    marginBottom: 20,
-    borderRadius: 15,
-    flexDirection: "row",
-    elevation: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: -2, height: 4 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  imageBox: {
-    width: 120,
-    height: 100,
-    backgroundColor: "gray",
-    borderTopLeftRadius: 10,
-    borderBottomLeftRadius: 10,
-  },
-  categoryBox: {
-    paddingLeft: 10,
-    width: 175,
-    height: 100,
-    justifyContent: "center",
-  },
+  addButton: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    backgroundColor: "#8C5674",
+    marginHorizontal: 65,
 
-
-
+  },
+  addButtonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
+  }
 });
