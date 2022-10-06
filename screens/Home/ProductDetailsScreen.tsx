fdfbd1ffb16c9cf3e715781c144e4884426927f2
yelab07 <79489573+yelab07@users.noreply.github.com>
2022-10-06 16:37:22 -0500

    Product detail page layout worked

--- screens/Home/ProductDetailsScreen.tsx
+++ screens/Home/ProductDetailsScreen.tsx
@@ -1,64 +1,88 @@
-import { StyleSheet, Text, View, ScrollView, Button, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/core";
 import { useProductQuery } from "../../services/productsApi";
-import { RouteProp } from "@react-navigation/native"
-type ParamList = { Params: { id: string }; };
+import { RouteProp } from "@react-navigation/native";
+import QuantitySelector from "../../components/QuantitySelector";
+import { scale } from "react-native-size-matters";
+
+type ParamList = { Params: { id: string } };
 type Props = {
   onPress: () => void;
 };
 
 const ProductDetailsScreen = ({ onPress }: Props) => {
   const navigation = useNavigation();
-  const route = useRoute<RouteProp<ParamList, "Params">>()
-  const { id } = route.params
+  const route = useRoute<RouteProp<ParamList, "Params">>();
+  const { id } = route.params;
   const { data } = useProductQuery(id);
+  const [quantity, setQuantity] = useState<any>(1);
 
-  // onPress=()=>(Alert.alert("Success","Product has beed added to the cart "))
+  const onAddToCart = () => { };
+
   return (
-
-
     <View style={styles.container}>
-
-
-
       <ScrollView>
-        < View style={{ alignItems: 'center', marginHorizontal: 0 }}>
-
-          <View >
-            <View >
+        <View style={{ alignItems: "center", marginHorizontal: 0 }}>
+          <View>
+            <View>
               <Text style={styles.title}>{data?.title}</Text>
             </View>
-            <View style={{ flexDirection: 'row', justifyContent: "space-between", marginBottom: 20 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: scale(20),
+                marginHorizontal: scale(20),
+              }}
+            >
               <Text style={styles.category}>{data?.category}</Text>
-              <Text> Rating:{data?.rating.rate}</Text>
+              <Text style={styles.rating}> Rating:{data?.rating.rate}</Text>
             </View>
           </View>
-          <View
-          >
-            <Image resizeMode="cover"
+          <View>
+            <Image
+              resizeMode="cover"
               style={styles.productImg}
               source={{ uri: `${data?.image}` }}
             />
           </View>
 
-          <View style={{ flexDirection: 'row', justifyContent: "space-between", flex: 1, width: 250 }}>
-            <View >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              flex: 1,
+              width: 250,
+              marginTop: 20,
+            }}
+          >
+            <View>
               <Text style={styles.price}> ${data?.price}</Text>
-              <Text style={{ fontSize: 20, alignContent: 'center' }}>In stock</Text>
-
+              <Text
+                style={{ fontSize: 20, alignContent: "center", color: "green" }}
+              >
+                In stock
+              </Text>
             </View>
             <View>
-              {/* <Text>Qty</Text> */}
-              {/* <QuantitySelector quantity={quantity} setQuantity={setQuantity} /> */}
+              <QuantitySelector quantity={quantity} setQuantity={setQuantity} />
             </View>
           </View>
-
+        </View>
+        <View>
+          <Text style={styles.description}>{data?.description}</Text>
         </View>
         <View style={styles.addToCartWrapper}>
-          <TouchableOpacity style={styles.addButton}
-          // onPress={()=>clickEventListener()}
-          >
+          <TouchableOpacity style={styles.addButton} onPress={onAddToCart}>
             <Text style={styles.addButtonText}>Add To Cart</Text>
           </TouchableOpacity>
         </View>
@@ -74,7 +98,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 30,
-    backgroundColor: "#dcb688"
+    backgroundColor: "#dcb688",
   },
   productImg: {
     width: 200,
@@ -82,30 +106,37 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   title: {
-    marginBottom: 10,
+    marginHorizontal: scale(20),
     fontSize: 25,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   category: {
     fontSize: 20,
+    textTransform: "capitalize",
+  },
+  rating: {
+    fontSize: 15,
+    marginVertical: scale(5),
   },
   price: { fontSize: 20 },
-  addToCartWrapper: {
-
+  addToCartWrapper: {},
+  description: {
+    marginVertical: scale(20),
+    lineHeight: scale(20),
+    marginHorizontal: scale(20),
   },
   addButton: {
-    marginTop: 10,
-    height: 45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: scale(10),
+    height: scale(45),
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 30,
     backgroundColor: "#8C5674",
     marginHorizontal: 65,
-
   },
   addButtonText: {
     color: "#FFFFFF",
     fontSize: 20,
-  }
+  },
 });
